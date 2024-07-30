0a9b51d6435f1e27a837ef09daacde5cc3e76c49
Can Kaynar <cannkaynarr@outlook.com>
2024-07-30 19:25:55 +0300

    Initial Commit

---
+++ Koin/KoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoinApp.swift
+//  Koin
+//
+//  Created by Can on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct KoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
